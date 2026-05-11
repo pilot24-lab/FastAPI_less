39d2439db0f3bc7d9d013aff1de8ddcc5696e75f
Nik <Nik>
2026-05-11 16:19:04 +0300

    Глава 1,2

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from fastapi-less!")
+from fastapi import FastAPI
+from fastapi.responses import Response
+from fastapi.exceptions import HTTPException
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+
+
+BALANCE = {}
+
+@app.get("/balance")
+def get_balance(wallet_name: str | None = None):
+    #Если имя кошелька не указано, возвращаем сумму балансов всех кошельков
+    if wallet_name is None:
+        return {"total_balance": sum(BALANCE.values())}
+    #Если кошелька нет в списке
+    if wallet_name not in BALANCE:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Wallet '{wallet_name}' not found"
+        )
+    #Возвращаем баланс конкретного кошелька 
+    return {"wallet": wallet_name, "balance": BALANCE[wallet_name]}
+
+@app.post("/wallets/{name}")
+def receive_money(name: str, amount: int):
+        #Если кошелька с таким именем еще нет - создаем его с балансом 0
+        if name not in BALANCE:
+             BALANCE[name] = 0
+        #Добавляем сумму к балансу кошелька
+        BALANCE[name] += amount
+        #Возвращаем инфу об операции
+        return {
+            "message": f"Added {amount} to {name}",
+            "wallet": name,
+            "new_balance": BALANCE[name]
+        }
